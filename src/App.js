f3202cc730bb034023b5971e9b2199f0edafb19a
MeSrinu <balisrinu1996@gmail.com>
2022-12-06 22:23:18 -0800

    react practise session

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Bsrinu from './components/bsrinu.jsx';
+
+class App extends React.Component {
+  render () {
+    return(
+      <div>
+        <Bsrinu/>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/bsrinu.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Bsrinu extends React.Component {
+    render (){
+        return (
+            <div>
+                <h1 style = {{color:'blueviolet' , fontFamily:  'serif', forntStyle:'normal', backgroundColor:'orange'}}>BALI SRINU</h1>
+                <p className='myself'>I am a web developer , i works omn the the jvascript and raectJS library.</p>
+                 
+            </div>
+        )
+    }
+}
+export default Bsrinu 
